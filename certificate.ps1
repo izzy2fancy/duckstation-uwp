778d3fb647433b57090930c7d3e198f746d43d91
Izzy Kauffman <121840901+izzy2fancy@users.noreply.github.com>
2024-03-10 22:50:17 -0400

    Update certificate.ps1

--- certificate.ps1
+++ certificate.ps1
@@ -1,6 +1,6 @@
 $PfxPath = './duckstation-uwp_TemporaryKey.pfx'
-$PfxBytes = [System.Convert]::FromBase64String('${{ secrets.DUCKSTATION_PFX }}') # Replace 'DUCKSTATION_PFX' with the name of your GitHub secret containing the Base64 encoded PFX file
+$PfxBytes = [System.Convert]::FromBase64String('${{ secrets.DUCKSTATION_PFX }}') # Replace 'DUCKSTATION_PFX' with the name of your GitHub secret containing the Base64-encoded PFX file
 [IO.File]::WriteAllBytes($PfxPath, $PfxBytes)
 
-$Password = ConvertTo-SecureString -String '${{ secrets.GITHUB_TOKEN }}' -AsPlainText -Force # Use GitHub token as the password
+$Password = ConvertTo-SecureString -String '${{ secrets.PFX_PASSWORD }}' -AsPlainText -Force # Use GitHub secret for the password
 Import-PfxCertificate -FilePath $PfxPath -CertStoreLocation Cert:\CurrentUser\My -Password $Password
