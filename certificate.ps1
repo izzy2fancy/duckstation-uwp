4846df18db3b6eb5122e6795d2e86759b60d9e12
Izzy Kauffman <121840901+izzy2fancy@users.noreply.github.com>
2024-03-10 20:15:04 -0400

    Add files via upload

--- certificate.ps1
+++ certificate.ps1
@@ -1,3 +1,3 @@
-$Pfxpath = './codesigning.pfx'
+$Pfxpath = './duckstation-uwp_TemporaryKey.pfx'
 $Password = ConvertTo-SecureString -String 'secret' -AsPlainText -Force
 Import-PfxCertificate -FilePath $Pfxpath -CertStoreLocation Cert:\CurrentUser\My -Password $Password
