9fc2eeb94ac3b66b878200256a1d8a5091643434
Izzy Kauffman <121840901+izzy2fancy@users.noreply.github.com>
2024-03-10 20:50:06 -0400

    Add files via upload

--- certificate.ps1
+++ certificate.ps1
@@ -1,3 +1,7 @@
-$Pfxpath = './duckstation-uwp_TemporaryKey.pfx'
-$Password = ConvertTo-SecureString -String 'secret' -AsPlainText -Force
-Import-PfxCertificate -FilePath $Pfxpath -CertStoreLocation Cert:\CurrentUser\My -Password $Password
+$PfxBase64 = '${{ secrets.YOUR_SECRET_NAME }}' # Replace 'DUCKSTATION_PFX' with the name of your GitHub secret containing the base64-encoded PFX data
+$PfxBytes = [System.Convert]::FromBase64String($PfxBase64)
+$PfxPath = './duckstation-uwp_TemporaryKey.pfx'
+[IO.File]::WriteAllBytes($PfxPath, $PfxBytes)
+
+$Password = ConvertTo-SecureString -String '${{ secrets.GITHUB_TOKEN }}' -AsPlainText -Force # Use GitHub token as the password
+Import-PfxCertificate -FilePath $PfxPath -CertStoreLocation Cert:\CurrentUser\My -Password $Password
